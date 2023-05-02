cf30de851d88b0c8dc5057e3dccd5e7ea2d8143f
JayceeBuarao <jayceebuarao@gmail.com>
2023-05-01 21:34:16 -0700

    populate data structure

---
+++ lib/data/questions.dart
@@ -0,0 +1,40 @@
+import 'package:quizapp/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'What does M stand for?',
+    [
+      'Mario',
+      'Mamamia',
+      'Mama mo',
+      'Michelle Obama',
+    ],
+  ),
+  QuizQuestion(
+    'Who is Mario brother?',
+    [
+      'Luigi',
+      'Wario',
+      'Bowser',
+      'Peaches',
+    ],
+  ),
+  QuizQuestion(
+    'What shape is Peaches bangs?',
+    [
+      'Heart',
+      'Rectangle',
+      'Toad',
+      'Mushroom',
+    ],
+  ),
+  QuizQuestion(
+    'What is Marios job?',
+    [
+      'Plumber',
+      'Model',
+      'Chauffeur',
+      'Barista',
+    ],
+  ),
+];

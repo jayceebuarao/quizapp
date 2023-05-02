727eac35db5a7d9660dde0e334f77865d58e7ed5
JayceeBuarao <jayceebuarao@gmail.com>
2023-05-01 21:34:23 -0700

    add widgets

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -10,6 +10,31 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('hello world');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('question'),
+          SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('answer 3'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('answer 4'),
+          ),
+        ],
+      ),
+    );
   }
 }
